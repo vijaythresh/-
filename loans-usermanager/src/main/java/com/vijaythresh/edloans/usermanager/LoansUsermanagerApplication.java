1430b40a5874e6cce381af0cc7ab7379c5bfdce3
vijaythresh <897453442@qq.com>
2018-08-18 14:47:33 +0800

    2

---
+++ loans-usermanager/src/main/java/com/vijaythresh/edloans/usermanager/LoansUsermanagerApplication.java
@@ -0,0 +1,12 @@
+package com.vijaythresh.edloans.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansUsermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansUsermanagerApplication.class, args);
+	}
+}
